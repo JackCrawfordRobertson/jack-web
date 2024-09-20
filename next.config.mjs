4e537ab36c40df8596787b1c3f5c68a452d18f2a
Jack Robetson <jackrobertsonps3@gmail.com>
2024-09-20 21:37:40 +0100

    Error Updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-    images: {
-        domains: ['res.cloudinary.com', 'example.com'], // Add multiple domains if necessary
-        deviceSizes: [320, 420, 768, 1024, 1200], // Sizes to be used for responsive images
-        imageSizes: [16, 32, 48, 64, 96], // Sizes to be used for fixed-size images
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
       },
-
-
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+        pathname: '/**',
+      },
+    ],
+    deviceSizes: [320, 420, 768, 1024, 1200], // Sizes to be used for responsive images
+    imageSizes: [16, 32, 48, 64, 96], // Sizes to be used for fixed-size images
+  },
 };
-
-
 
 export default nextConfig;
--- src/app/components/SmoothScrollProvider.js
+++ src/app/components/SmoothScrollProvider.js
@@ -1,24 +1,33 @@
 'use client'; // Mark this as a client-side component
 
 import { useEffect } from 'react';
-import LocomotiveScroll from 'locomotive-scroll';
-import 'locomotive-scroll/dist/locomotive-scroll.css'; // Import locomotive-scroll CSS
 
 const SmoothScrollProvider = ({ children }) => {
   useEffect(() => {
-    // Ensure this only runs on the client side
+    // Ensure this code runs only on the client side
     if (typeof window === 'undefined') return;
 
-    const scrollContainer = document.querySelector('#scroll-container');
+    let scroll;
 
-    const scroll = new LocomotiveScroll({
-      el: scrollContainer,
-      smooth: true,
-      smoothMobile: true, // Enables smooth scrolling on mobile devices
+    // Dynamically import LocomotiveScroll and CSS
+    Promise.all([
+      import('locomotive-scroll'),
+      import('locomotive-scroll/dist/locomotive-scroll.css'),
+    ]).then(([LocomotiveScrollModule]) => {
+      const LocomotiveScroll = LocomotiveScrollModule.default;
+
+      const scrollContainer = document.querySelector('#scroll-container');
+
+      scroll = new LocomotiveScroll({
+        el: scrollContainer,
+        smooth: true,
+        smoothMobile: true, // Enables smooth scrolling on mobile devices
+      });
     });
 
+    // Clean up when the component is unmounted
     return () => {
-      if (scroll) scroll.destroy(); // Clean up when the component is unmounted
+      if (scroll) scroll.destroy();
     };
   }, []);
 
